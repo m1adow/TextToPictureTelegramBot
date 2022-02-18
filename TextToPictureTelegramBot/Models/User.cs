f8e7493e11392df45ec1943b2b8c8c26ebd5dcbb
m1adow <Vell0505189970sl@gmail.com>
2022-02-18 20:36:10 +0200

    Added state machine pattern

---
+++ TextToPictureTelegramBot/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextToPictureTelegramBot.Models
+{
+    internal class User
+    {
+        public UserState State { get; set; }
+        public long ChatId { get; set; }
+        public Color BackgroundColor { get; set; }
+        public Color TextColor { get; set; }
+        public Color HeartColor { get; set; }
+    }
+
+    internal enum UserState
+    {
+        Basic = 0,
+        EnterBackgroundColor = 1,
+        EnterTextColor = 2,
+        EnterHeartColor = 3
+    }
+}
--- TextToPictureTelegramBot/PictureForm.cs
+++ TextToPictureTelegramBot/PictureForm.cs
@@ -2,6 +2,7 @@ using Telegram.Bot;
 using System.Drawing.Imaging;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types.InputFiles;
+using TextToPictureTelegramBot.Models;
 
 namespace TextToPictureTelegramBot
 {
@@ -10,6 +11,7 @@ namespace TextToPictureTelegramBot
         public string Token { get; private set; } = "1898319362:AAGbrOnEPt7coIUk_sg7txKBqGBaR4cFl28";
         private string _path = $@"{Environment.CurrentDirectory}/picture.jpg";
         private TelegramBotClient? _client;
+        private List<User> _users = new();
 
         public PictureForm()
         {
@@ -37,9 +39,80 @@ namespace TextToPictureTelegramBot
         {
             try
             {
+                User? currentUser = _users.FirstOrDefault(u => u.ChatId == e.Message.Chat.Id);
+
+                if (currentUser is null)
+                {
+                    currentUser = new User
+                    {
+                        ChatId = e.Message.Chat.Id,
+                        State = UserState.Basic,
+                        BackgroundColor = Color.White,
+                        TextColor = Color.Black
+                    };
+                }
+
                 var message = e.Message;
 
-                if (message.Text != null && message.Text[0] != '/')
+                if (currentUser.State == UserState.Basic)
+                {
+                    if (message.Text != null && message.Text[0] != '/')
+                    {
+                        this.BackColor = currentUser.BackgroundColor;
+                        this.labelText.ForeColor = currentUser.TextColor;
+
+                        labelText.Invoke((MethodInvoker)delegate
+                        {
+                            labelText.Text = message.Text.ToUpper();
+                            labelText.Left = (this.ClientSize.Width - labelText.Width) / 2;
+                            labelText.Top = (this.ClientSize.Height - labelText.Height) / 2;
+                        });
+
+                        pictureBox.Invoke((MethodInvoker)delegate
+                        {
+                            pictureBox.Left = (this.ClientSize.Width - pictureBox.Width) / 2;
+                            pictureBox.Top = (this.ClientSize.Height - pictureBox.Height) / 2;
+                        });
+
+                        GetScreenShot().Save(_path, ImageFormat.Jpeg);
+
+                        using (var stream = File.Open(_path, FileMode.Open))
+                        {
+                            InputOnlineFile file = new(stream);
+                            file.FileName = "picture.jpg";
+                            await _client.SendDocumentAsync(message.Chat.Id, file);
+                        }
+                    }
+                    else if (message.Text != null && message.Text[0] == '/')
+                    {
+                        switch (message.Text)
+                        {
+                            case "/background":
+                                currentUser.State = UserState.EnterBackgroundColor;
+                                await _client.SendTextMessageAsync(currentUser.ChatId, "Choose color of background: Red, Green, Blue, White, Black");
+                                return;
+                            case "/textcolor":
+                                return;
+                            case "/heart":
+                                return;
+                            default:
+                                await _client.SendTextMessageAsync(currentUser.ChatId, "Bot doesn't exist this command");
+                                return;
+                        }
+                    }
+                }
+
+                if (currentUser.State == UserState.EnterBackgroundColor)
+                {
+                    ChangeBackgroundColor(_client, currentUser, e);
+                }
+
+                if(currentUser.State == UserState.EnterTextColor)
+                {
+                    ChangeTextColor(_client, currentUser, e);
+                }
+
+                /*if (message.Text != null && message.Text[0] != '/')
                 {
                     labelText.Invoke((MethodInvoker)delegate
                     {
@@ -60,7 +133,7 @@ namespace TextToPictureTelegramBot
                     {
                         InputOnlineFile file = new(stream);
                         file.FileName = "picture.jpg";
-                        var send = await _client.SendDocumentAsync(message.Chat.Id, file);
+                        await _client.SendDocumentAsync(message.Chat.Id, file);
                     }
 
                 }
@@ -84,9 +157,59 @@ namespace TextToPictureTelegramBot
                         await _client.SendTextMessageAsync(message.Chat.Id, "Choose color of heart: Red, Rose, White, Black\nFor delete: None");
                         _client.OnMessage += OnHeartHandler;
                     }
-                }
+                }*/
             }
             catch (Exception ex) { }
+        }
+
+        private async void ChangeBackgroundColor(TelegramBotClient client, User user, MessageEventArgs e)
+        {
+            switch (e.Message.Text.ToUpper())
+            {
+                case "RED":
+                    user.BackgroundColor = Color.Red;
+                    return;
+                case "GREEN":
+                    user.BackgroundColor = Color.Green;
+                    return;
+                case "BLUE":
+                    user.BackgroundColor = Color.Blue;
+                    return;
+                case "WHITE":
+                    user.BackgroundColor = Color.White;
+                    return;
+                case "BLACK":
+                    user.BackgroundColor = Color.Black;
+                    return;
+                default:
+                    await client.SendTextMessageAsync(user.ChatId, "You can't choose this color.");
+                    return;
+            }
+        }
+
+        private async void ChangeTextColor(TelegramBotClient client, User user, MessageEventArgs e)
+        {
+            switch (e.Message.Text.ToUpper())
+            {
+                case "RED":
+                    user.TextColor = Color.Red;
+                    return;
+                case "GREEN":
+                    user.TextColor = Color.Green;
+                    return;
+                case "BLUE":
+                    user.TextColor = Color.Blue;
+                    return;
+                case "WHITE":
+                    user.TextColor = Color.White;
+                    return;
+                case "BLACK":
+                    user.TextColor = Color.Black;
+                    return;
+                default:
+                    await client.SendTextMessageAsync(user.ChatId, "You can't choose this color.");
+                    return;
+            }
         }
 
         private async void OnBackGroundHandler(object sender, MessageEventArgs e)
